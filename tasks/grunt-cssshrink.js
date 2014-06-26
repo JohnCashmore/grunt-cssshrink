8aaffbfbcdd3bfca3d97148f233f961eb0c038ae
John Cashmore <cashy@whoooop.co.uk>
2014-06-26 09:02:14 +0100

    initial build

---
+++ tasks/grunt-cssshrink.js
@@ -0,0 +1,80 @@
+/*
+ * grunt-combine-media-queries
+ * https://github.com/buildingblocks/grunt-combine-media-queries
+ *
+ * Copyright (c) 2013 John Cashmore
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.registerMultiTask('cssshrink', 'Shrinks css.', function() {
+
+    // Require stuff
+    var parseCss = require('cssshrink');
+    var path = require('path');
+    var error = true;
+
+    // Default options
+    var options = this.options({
+      log: false,
+      ext: false
+    });
+
+    // Log info only when 'options.log' is set to true
+    var log = function(message){
+      if (options.log){
+        grunt.log.writeln(message);
+      }
+    };
+
+
+    this.files.forEach(function(f) {
+
+      f.src.forEach(function (filepath) {
+
+        error = false;
+
+        log('\nFile ' + filepath + ' found.');
+
+        var destpath = f.dest;
+        var filename = filepath.replace(/(.*)\//gi, '');
+
+        if (destpath.indexOf(filename) === -1) {
+          destpath = path.join(f.dest, filename);
+        }
+
+        var source = grunt.file.read(filepath);
+        
+
+        var outputcss = cssshrink.shrink(source);
+
+        
+
+        
+
+        // Define the new file extension
+        if( options.ext ){
+          destpath = destpath.replace( /\.(.*)/ , options.ext);
+        }
+
+        // Normalize line endings
+        outputcss = grunt.util.normalizelf(outputcss);
+
+        // Write the new file
+        grunt.file.write(destpath, outputcss);
+        grunt.log.ok('File ' + destpath + ' created.');
+
+      });
+
+      if(error){
+        grunt.fatal('No files found');
+      }
+
+    });
+
+  });
+
+};
